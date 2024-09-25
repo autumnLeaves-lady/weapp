a2bae9d70b113eba7d419a24ba1b9f6a706e5ad0
autumnLeaves-lady <1962500337@qq.com>
2024-09-25 23:13:13 +0800

    提交

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
+import AddressPanel from './components/AddressPanel.vue'
+import ServicePanel from './components/ServicePanel.vue'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -42,6 +44,13 @@ const popup = ref<{
   open: (type?: UniHelper.UniPopupType) => void
   close: () => void
 }>()
+
+// 弹出层条件渲染
+const popupName = ref<'address' | 'service'>()
+const openPopup = (name: typeof popupName.value) => {
+  popupName.value = name
+  popup.value?.open()
+}
 </script>
 
 <template>
@@ -78,11 +87,11 @@ const popup = ref<{
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view @tap="popup?.open('bottom')" class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -154,9 +163,9 @@ const popup = ref<{
   </view>
 
   <!-- uni-ui弹出层 -->
-  <uni-popup ref="popup" background-color="#fff">
-    <view>底部弹出 Popup 自定义圆角</view>
-    <button @tap="popup?.close">关闭弹出层</button>
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close" />
   </uni-popup>
 </template>
 
